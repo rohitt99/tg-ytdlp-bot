47dc18e9db438e7a31267df0cfb25962964090db
ratu.sh <69438111+chelaxian@users.noreply.github.com>
2025-08-06 22:20:17 +0300

    Update messages.py

--- CONFIG/messages.py
+++ CONFIG/messages.py
@@ -53,7 +53,7 @@ To download playlists send its URL with <code>*start*end</code> ranges in the en
 🟧 <b>Pornhub models:</b>
 <code>https://www.pornhub.org/model/MODEL_NAME*1*2</code>
 (downloads first 2 video from model profile)
-<code>https://www.pornhub.com/video/search?search=YOUR_PROMPT*1*3</code>
+<code>https://www.pornhub.com/video/search?search=YOUR+PROMPT*1*3</code>
 (downloads first 3 video from search results by your prompt)
 
 and so on...
